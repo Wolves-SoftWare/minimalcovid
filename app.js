6763a9cfd5459b8f892585000ee645e3b3df5e32
zechaos031 <delrieu.adrien@gmail.com>
2021-03-01 13:55:04 +0100

    :wrench: Make update on lunch

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+require('./job')
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
